0a7d8dbdea2450a14cd710f6639a6104732d499a
php 17 <php.17@metralha.cec.ime.usp.br>
2017-02-06 22:43:20 -0200

    adicionando exercicio 5

--- logica/Aula1Ex5.php
+++ logica/Aula1Ex5.php
@@ -1,2 +1,9 @@
 <?php
-echo "*\n**\n***\n****\n*****\n";
+
+for ($i=0; $i < 6; $i++) {
+  for ($j=0; $j < $i; $j++) {
+    $linha .= "*"; //$linha = $linha."*"
+  }
+  echo "$linha\n";
+  $linha = "";
+}
