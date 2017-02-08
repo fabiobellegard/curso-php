4acd35267a6c989eaa44042be6cb836809db03d0
php 17 <php.17@metralha.cec.ime.usp.br>
2017-02-07 22:19:16 -0200

    inserindo programas de agregacao

---
+++ agregacao/Aula3Ex1.php
@@ -0,0 +1,12 @@
+<?php
+
+Class Cliente{
+  public $nome;
+  public $codigo;
+}
+
+Class CartaoDeCredito{
+  public $numero;
+  public $datadeValidade;
+  public $cliente;
+}
---
+++ agregacao/Aula3Ex2.php
@@ -0,0 +1,23 @@
+<?php
+
+Class Cliente{
+  public $nome;
+  public $codigo;
+}
+
+Class CartaoDeCredito{
+  public $numero;
+  public $datadeValidade;
+  public $dono;
+}
+
+$cliente = new Cliente;
+$cliente->nome = 'Rafael Cosentino';
+$cliente->codigo = 123;
+
+$cartao = new CartaoDeCredito;
+$cartao->numero = 111111;
+$cartao->datadeValidade = '12/12/18';
+$cartao->dono = $cliente;
+
+echo "Nome do dono do cartão: {$cartao->dono->nome}\n";
