a8d172adc37170d292375ab51f68c4b96f0e5ee2
php 17 <php.17@metralha.cec.ime.usp.br>
2017-02-09 21:04:58 -0200

    adicionando atributos

---
+++ atributos/Conta.php
@@ -0,0 +1,13 @@
+<?php
+
+class Conta{
+
+  public $numero;
+  public $saldo;
+  public $contador;
+
+  public function __construct(){
+
+    $this->contador++;
+  }
+} ?>
---
+++ atributos/TestarConta.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'Conta.php';
+
+$x = new Conta;
+$x->numero = 11;
+$x->saldo = 600;
+
+$y = new Conta;
+$y->numero = 12;
+$y->saldo = 130;
